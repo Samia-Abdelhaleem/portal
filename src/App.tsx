4b2e6c54ba59c080c41606d7957c79720bcea2cb
Samia-Abdelhaleem <samiaabdelhaleem@gmail.com>
2023-12-06 23:33:43 +0200

    add portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,118 @@
-import React from 'react';
+import React from "react";
 
-import UserCard from './components/UserCard';
-export interface PendingId{
-   id : number
-  fields :{title : string ,value : string | number}[]
-  idFront : string
-  idBack : string
+import UserCard from "./components/UserCard";
+export interface PendingId {
+  id: number;
+  fields: { title: string; value: string | number }[];
+  idFront: string;
+  idBack: string;
 }
 
 function App() {
-
-  const pendingIds : PendingId[] =[{
-    id : 1,
-    fields :  [{
-    title :'Name',
-    value :'Samia'
-  },
-{
-    title :'Job',
-    value :'Career'
-  },
-{
-    title :'Name',
-    value :'Samia'
-  },
-{
-    title :'Job',
-    value :'Career'
-  }],
-  idFront :"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg",
-  idBack:"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg"
-  },
-{
-    id : 2,
-    fields :  [{
-    title :'Name',
-    value :'Samia'
-  },
-{
-    title :'Job',
-    value :'Career'
-  }],
-  idFront :"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg",
-  idBack:"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg"
-  },
-{
-    id : 3,
-    fields :  [{
-    title :'Name',
-    value :'Samia'
-  },
-{
-    title :'Job',
-    value :'Career'
-  },
-{
-    title :'Name',
-    value :'Samia'
-  },
-{
-    title :'Job',
-    value :'Career'
-  }],
-  idFront :"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg",
-  idBack:"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg"
-  },
-{
-    id : 4,
-    fields :  [{
-    title :'Name',
-    value :'Samia'
-  },
-{
-    title :'Job',
-    value :'Career'
-  }],
-  idFront :"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg",
-  idBack:"https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg"
-  }]
+  const pendingIds: PendingId[] = [
+    {
+      id: 1,
+      fields: [
+        {
+          title: "Name",
+          value: "Samia",
+        },
+        {
+          title: "Job",
+          value: "Career",
+        },
+        {
+          title: "Name",
+          value: "Samia",
+        },
+        {
+          title: "Job",
+          value: "Career",
+        },
+      ],
+      idFront:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3u2qiqt7TJhBKHbDiD802G3HvbFukL14yHtHUw_MCw&s",
+      idBack:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyIfV9_gZP6IxiWdFyL6KwzRA0E0j0OJDg0vsCEYdmkGXxmauMY7lFlapjGiRwi-t0l-4&usqp=CAUg",
+    },
+    {
+      id: 2,
+      fields: [
+        {
+          title: "Name",
+          value: "Samia",
+        },
+        {
+          title: "Job",
+          value: "Career",
+        },
+      ],
+       idFront:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3u2qiqt7TJhBKHbDiD802G3HvbFukL14yHtHUw_MCw&s",
+      idBack:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyIfV9_gZP6IxiWdFyL6KwzRA0E0j0OJDg0vsCEYdmkGXxmauMY7lFlapjGiRwi-t0l-4&usqp=CAUg",
+   },
+    {
+      id: 3,
+      fields: [
+        {
+          title: "Name",
+          value: "Samia",
+        },
+        {
+          title: "Job",
+          value: "Career",
+        },
+        {
+          title: "Name",
+          value: "Samia",
+        },
+        {
+          title: "Job",
+          value: "Career",
+        },
+      ],
+       idFront:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3u2qiqt7TJhBKHbDiD802G3HvbFukL14yHtHUw_MCw&s",
+      idBack:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyIfV9_gZP6IxiWdFyL6KwzRA0E0j0OJDg0vsCEYdmkGXxmauMY7lFlapjGiRwi-t0l-4&usqp=CAUg",
+   
+    },
+    {
+      id: 4,
+      fields: [
+        {
+          title: "Name",
+          value: "Samia",
+        },
+        {
+          title: "Job",
+          value: "Career",
+        },
+      ],
+        idFront:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3u2qiqt7TJhBKHbDiD802G3HvbFukL14yHtHUw_MCw&s",
+      idBack:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyIfV9_gZP6IxiWdFyL6KwzRA0E0j0OJDg0vsCEYdmkGXxmauMY7lFlapjGiRwi-t0l-4&usqp=CAUg",
+   
+    },
+  ];
   return (
-  <>  <div className='header'><img src="images/logo.jpg" alt="" className='header__image' /></div>
-  <div className='users__content'>
-    {pendingIds.map((pendingId)=> <UserCard key={pendingId.id}  pendingId={pendingId}></UserCard>)}
-  
-  
-  </div></>
+    <div className="pending__ids__container">
+      <div className="header">
+        <img src="images/logo.jpg" alt="" className="header__image" />
+      </div>
+      <div className="pending__ids">
+        <div className="pending__ids__title">  <span>pending IDs</span></div>
+        <div className="content">
+          <div className="users__content">
+            {pendingIds.map((pendingId) => (
+              <UserCard key={pendingId.id} pendingId={pendingId}></UserCard>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -13,23 +13,31 @@ const UserCard  = ({pendingId } :  PropsData) => {
 
 
   return (
+  
     <div className="card__container">
-      <div className="card__info"> {fields.map((field, index)=>{
-        return <div key={`${field.title}/${field.value}/${index}`}>
-          <span> {field.title} : </span>
-          <span> {field.value}</span>
-        </div>
-      })}</div>
-      <div className="card__images"> <div className="image__frame">
+       <div className="card__images"> <div className="image__frame">
         <img src={idFront} alt=" front Id" className="image" /></div>
         <div className="image__frame">
           <img src={idBack} alt="back Id" className="image"  />
           </div> </div>
-      <div className="card__footer">
+      <div className="card__info"> <div className="content" >
+        {fields.map((field, index)=>{
+        return <div className="content__data" key={`${field.title}/${field.value}/${index}`}>
+          <span className="card__info__label"> {field.title} :  </span>
+          <span> {field.value}</span>
+        </div>
+      })}</div>   <div className="card__footer">
         <button onClick={()=>executeAction(id, 1)} className="action-btn"> Approve</button>
         <button onClick={()=> executeAction(id, 0)} className="action-btn reject-btn"> Reject</button>
-      </div>
+      
+      
+      </div></div>
+     
+         
+     
     </div>
+    
+   
   );
 };
 
